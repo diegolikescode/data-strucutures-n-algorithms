3242e5b7c19e0093b2bb77156df18fda04ca4396
diegolikescode <diegoprestes.git@hotmail.com>
2025-02-25 09:29:31 -0300

    lets GO[lang]

--- src/algorithms/binary-search/binary-search.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef BINARY_SEARCH_H
-#define BINARY_SEARCH_H
-
-#include <stddef.h>
-
-int binary_search(int* arr, size_t arr_len, int v);
-
-#endif
--- src/algorithms/crystal-ball/crystal-ball.h
+++
@@ -1,8 +0,0 @@
-#ifndef BINARY_SEARCH_H
-#define BINARY_SEARCH_H
-
-#include <stddef.h>
-
-int find_index_crystal_ball_breaks(int* arr, size_t len);
-
-#endif
